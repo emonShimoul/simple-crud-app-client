6c74336ec2a0ac521d86cb5856c00ea2df526a4b
emonShimoul <emonshimoul@gmail.com>
2025-02-24 03:28:18 +0600

    load and display data from db

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import "./index.css";
 import App from "./App.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Users from "./components/Users.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/users",
+    element: <Users></Users>,
+    loader: () => fetch("http://localhost:5000/users"),
   },
 ]);
 
