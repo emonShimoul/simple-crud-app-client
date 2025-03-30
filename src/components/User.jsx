4fb40f724b1895ac729cda430576d70f504cfefc
emonShimoul <emonshimoul@gmail.com>
2025-03-30 23:25:46 +0600

    user table management

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,14 +1,19 @@
 const User = ({ user, handleDelete }) => {
+  //   console.log(user);
+
   return (
     <tr>
       <th>1</th>
       <td>Cy Ganderton</td>
       <td>{user.email}</td>
       <td className="flex gap-2">
-        <button className="btn btn-xs btn-success" onClick={handleDelete}>
-          Update
+        <button className="btn btn-xs btn-success">Update</button>
+        <button
+          className="btn btn-xs btn-error text-white"
+          onClick={() => handleDelete(user.id)}
+        >
+          Delete
         </button>
-        <button className="btn btn-xs btn-error text-white">Delete</button>
       </td>
     </tr>
   );
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -6,18 +6,20 @@ const Users = () => {
   const loadedUsers = useLoaderData();
   const [users, setUsers] = useState(loadedUsers);
 
-  const handleDelete = (_id) => {
-    fetch(`http://localhost:5000/users/${_id}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.deletedCount > 0) {
-          alert("Deleted Successfully!!");
-          const remaining = users.filter((user) => user._id !== _id);
-          setUsers(remaining);
-        }
-      });
+  const handleDelete = (id) => {
+    console.log("Deleted ID: ", id);
+
+    // fetch(`http://localhost:5000/users/${_id}`, {
+    //   method: "DELETE",
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     if (data.deletedCount > 0) {
+    //       alert("Deleted Successfully!!");
+    //       const remaining = users.filter((user) => user._id !== _id);
+    //       setUsers(remaining);
+    //     }
+    //   });
   };
   return (
     <div className="overflow-x-auto">
